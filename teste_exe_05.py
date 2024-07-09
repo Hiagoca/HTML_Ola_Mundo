c518a0400d59fd327e75689861904f418c3c97da
Hiagoca <hiagoca.senac@gmail.com>
2024-07-09 19:24:06 -0300

    Add files via upload

---
+++ teste_exe_05.py
@@ -0,0 +1,4 @@
+var_int = int(input("Digite um número inteiro: "))
+var_float = float(input("Digite um número decimal: "))
+var_palavra = str(input("Digite uma palavra: "))
+print(f"As informação digitadas foram: {var_int}, {var_float}, {var_palavra}")
